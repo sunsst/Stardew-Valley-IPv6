c6fb9b34ea27926b20cf578758e897191b374329
abab <abab@cc.com>
2025-07-19 14:06:24 +0800

    兼容安卓

--- IPv6/Patch/MyPatch.Patching.cs
+++ IPv6/Patch/MyPatch.Patching.cs
@@ -50,14 +50,36 @@ namespace IPv6.Patch
         }
 
 
-        public static void Patching(IModHelper helper, string harmonyID)
+        public static void Patching(IModHelper helper , string harmonyID)
         {
             InitRefValues(helper, harmonyID);
             //Harmony.DEBUG = true;
 
+            // 该类只在Android上有效
+            var menuType = AccessTools.TypeByName("StardewValley.Menus.CoopGameMenu");
+
+            if (menuType == null)
+            {
+                GameLog("IPv6: Start on PC");
+                menuType = AccessTools.TypeByName("StardewValley.Menus.CoopMenu");
+                MyPatch.PatchingGame(helper, harmonyID, menuType);
+            }
+            else
+            {
+                GameLog("IPv6: Start on Android");
+                MyPatch.PatchingAndroid(helper, harmonyID, menuType);
+            }
+            
+        }
+
+
+        public static void PatchingGame(IModHelper helper, string harmonyID, Type menuType)
+        {
+            // 该方法用于为PC版本SMAPI添加补丁
+
             foreach (var m in new MethodBase[] {
            AccessTools.Method(typeof(StardewValley.Game1), "UpdateTitleScreen"),
-           AccessTools.Method(typeof(StardewValley.Menus.CoopMenu), "enterIPPressed"),
+           AccessTools.Method(menuType, "enterIPPressed"),
            AccessTools.Method(typeof(StardewValley.Multiplayer), "LogDisconnect")})
             {
                 Harmony.Patch(m, transpiler: ClientTranspiler);
@@ -67,5 +89,33 @@ namespace IPv6.Patch
 
             Harmony.Patch(AccessTools.Method(typeof(StardewValley.Network.GameServer), "UpdateLocalOnlyFlag"), transpiler: UpdateLocalOnlyFlagTranspiler);
         }
+
+
+        private static void TitleTextInputMenu_Postfix(StardewValley.Menus.TitleTextInputMenu __instance, string title, StardewValley.Menus.NamingMenu.doneNamingBehavior b, string default_text, string context, bool filterInput)
+        {
+            __instance.textBox.textLimit = 100;
+        }
+        public static void PatchingAndroid(IModHelper helper, string harmonyID, Type menuType)
+        {
+            // 该方法用于为Android版本SMAPI添加补丁
+
+            foreach (var m in new MethodBase[] {
+           AccessTools.Method(typeof(StardewValley.Game1), "UpdateTitleScreen"),
+           AccessTools.Method(menuType, "enterIPPressed"),
+           AccessTools.Method(typeof(StardewValley.Multiplayer), "LogDisconnect")})
+            {
+                Harmony.Patch(m, transpiler: ClientTranspiler);
+            }
+
+            var original = AccessTools.Constructor(typeof(StardewValley.Menus.TitleTextInputMenu), new Type[] { typeof(string), typeof(StardewValley.Menus.NamingMenu.doneNamingBehavior), typeof(string), typeof(string), typeof(bool) });
+            var postfix = typeof(MyPatch).GetMethod(nameof(TitleTextInputMenu_Postfix), BindingFlags.Static | BindingFlags.NonPublic);
+            harmony.Patch(original, postfix: new HarmonyMethod(postfix));
+
+
+            Harmony.Patch(AccessTools.Constructor(typeof(StardewValley.Network.GameServer), new Type[] { typeof(bool) }), transpiler: ServerTranspiler);
+
+            Harmony.Patch(AccessTools.Method(typeof(StardewValley.Network.GameServer), "UpdateLocalOnlyFlag"), transpiler: UpdateLocalOnlyFlagTranspiler);
+        }
+
     }
 }
